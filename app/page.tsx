7ea932e9d3e0885a8b75ea05b988a71df6e9ad83
Wonseok Yang <wsyang@digicaps.com>
2024-04-04 13:33:13 +0900

    how to use shadcn-ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
-import Image from 'next/image'
+import { Button } from '@/components/ui/button'
 
 export default function Home() {
-  return <p className="font-semibold text-green-500">Hello Auth!</p>
+  return (
+    <Button size="lg" variant="custom">
+      Click me
+    </Button>
+  )
 }
