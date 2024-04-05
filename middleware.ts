dfcb4ffce28cd479a4814a843566c046dc77fb38
Wonseok Yang <wsyang@digicaps.com>
2024-04-05 17:49:11 +0900

    middleware setup to handle route branching with auth

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,33 @@
 import NextAuth from 'next-auth'
 
-import authConfig from './auth.config'
+import authConfig from '@/auth.config'
+import { DEFAULT_LOGIN_REDIRECT, apiAuthPrefix, authRoutes, publicRoutes } from '@/routes'
 
 const { auth } = NextAuth(authConfig)
 
 export default auth((req) => {
+  const { nextUrl } = req
   const isLoggedIn = !!req.auth
-  console.log('ROUTE: ', req.nextUrl.pathname)
-  console.log('IS LOGGEDIN: ', isLoggedIn)
+
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix)
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname)
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname)
+
+  if (isApiAuthRoute) {
+    return
+  }
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
+    }
+    return
+  }
+
+  if (!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL('/auth/login', nextUrl))
+  }
+  return
 })
 
 // Optionally, don't invoke Middleware on some paths
