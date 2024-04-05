4c513243a1db6c275da2697d65641e4e6bea9a22
Wonseok Yang <wsyang@digicaps.com>
2024-04-05 15:57:24 +0900

    add user row in database

---
+++ data/user.ts
@@ -0,0 +1,17 @@
+import { db } from '@/lib/db'
+
+export const getUserByEmail = async (email: string) => {
+  try {
+    const user = await db.user.findUnique({ where: { email } })
+  } catch (err) {
+    return null
+  }
+}
+
+export const getUserById = async (id: string) => {
+  try {
+    const user = await db.user.findUnique({ where: { id } })
+  } catch (err) {
+    return null
+  }
+}
