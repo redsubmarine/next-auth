2f745a6b8c94abe90f5cf785d2e31be1ef694606
Wonseok Yang <wsyang@digicaps.com>
2024-04-08 16:31:18 +0900

    imple to login with google/github

--- auth.ts
+++ auth.ts
@@ -11,6 +11,10 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  pages: {
+    signIn: '/auth/login',
+    error: '/auth/error',
+  },
   callbacks: {
     /*
     async signIn({ user }) {
