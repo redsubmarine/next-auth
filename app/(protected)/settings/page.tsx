f6bd6737ffd45b386f9912acea53d78cd4626e10
red <wonseok@duck.com>
2024-04-13 18:53:54 +0900

    add navbar and some buttons

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,25 +1,22 @@
 'use client'
 
 import { logout } from '@/actions/logout'
-import { useSession } from 'next-auth/react'
+import { useCurrentUser } from '@/hooks/use-current-user'
 import { FunctionComponent } from 'react'
 
 interface SettingsProps {}
 
 const Settings: FunctionComponent<SettingsProps> = () => {
-  const session = useSession()
+  const user = useCurrentUser()
 
   const onClick = () => {
     logout()
   }
   return (
-    <div>
-      {JSON.stringify(session)}
-      <form>
-        <button onClick={onClick} type="submit">
-          Sign out
-        </button>
-      </form>
+    <div className="bg-white p-10 rounded-xl">
+      <button onClick={onClick} type="submit">
+        Sign out
+      </button>
     </div>
   )
 }
